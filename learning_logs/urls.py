0dc6b60e2227fffac4b306297f0242bd30b45564
Bill Cathey <billcathey@hotmail.com>
2023-02-06 16:38:54 -0500

    Implement add new entry functionality

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     # Detail page for a single topic
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     # Page for adding new topic
-    path('new_topic/', views.new_topic, name='new_topic')
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """ The home page for Learning Log. """
@@ -33,3 +33,21 @@ def new_topic(request):
         # Display blank or invalid form
         context = {'form': form}
         return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """ Add a new entry for a particular topic. """
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # No data submitted; create blank form
+        form = EntryForm()
+    else:
+        # POST data submitted; process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+        # Display blank or invalid form
+        context = {'topic': topic, 'form': form}
+        return render(request, 'learning_logs/new_entry.html', context)
