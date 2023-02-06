463184ee9a3c912dc1c545bb04b099858191bbc5
Bill Cathey <billcathey@hotmail.com>
2023-02-05 22:29:51 -0500

    Delete space in date filter in topic template causing error

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -14,6 +14,6 @@ def topics(request):
 def topic(request, topic_id):
     """ Show a single topic and all its entries. """
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('date_added')
+    entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
