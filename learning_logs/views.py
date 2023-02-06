6a6cb72b8b6bb03050bbc51c0cefcc296d96dc6a
Bill Cathey <billcathey@hotmail.com>
2023-02-05 21:18:41 -0500

    Define topics view function

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """ The home page for Learning Log. """
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ Show all topics. """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
