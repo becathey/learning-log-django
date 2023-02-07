bf8765349eaca613436c07bdd4f7f5fd353866e0
Bill Cathey <billcathey@hotmail.com>
2023-02-06 21:33:07 -0500

    Restrict views to logged in users

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -120,3 +120,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_URL = 'users:login'
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -6,12 +7,14 @@ def index(request):
     """ The home page for Learning Log. """
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """ Show all topics. """
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """ Show a single topic and all its entries. """
     topic = Topic.objects.get(id=topic_id)
@@ -19,6 +22,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """ Add a new topic. """
     if request.method != 'POST':
@@ -34,6 +38,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """ Add a new entry for a particular topic. """
     topic = Topic.objects.get(id=topic_id)
@@ -52,6 +57,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """ Edit existing entry. """
     entry = Entry.objects.get(id=entry_id)
