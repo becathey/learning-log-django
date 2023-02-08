20e2d652e2d36daf7181fe45b058ad9d11b9e92f
Bill Cathey <billcathey@hotmail.com>
2023-02-08 18:59:28 -0500

    Apply styling to new entry template

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -11,5 +11,5 @@ class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['text']
-        labels = {'text': 'Entry:'}
+        labels = {'text': ''}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
